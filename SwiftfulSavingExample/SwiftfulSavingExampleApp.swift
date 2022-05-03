7ca9f11308a8e3a9bad30bbf53723c2b3232acb4
Swiftful Thinking <ndsarno16@gmail.com>
2022-05-02 22:16:47 -0400

    Initial Commit

---
+++ SwiftfulSavingExample/SwiftfulSavingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSavingExampleApp.swift
+//  SwiftfulSavingExample
+//
+//  Created by Nick Sarno on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSavingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
